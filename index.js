66f7c0cf4261057319dd4ca91c2b6d7083796584
Jéssica Mazoni <jessicamazonim@gmail.com>
2019-04-13 00:12:41 -0300

    Projeto Certo e finalizado

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
-const handlebars = require('express-handlebars')// Usei para criar o template
+const path = require('path');
+__dirname = path.resolve();
+const handlebars = require('express-handlebars')// Usado para criar o template
 const bodyParser = require('body-parser')
 const Post = require('./models/Post')
 
 // Config
- // Template Engine rsrsrs
+ // Template Engine 
 
 app.engine('handlebars',handlebars({defaultLayout: 'main'}))
 app.set('view engine','handlebars')
@@ -15,12 +17,19 @@ app.set('view engine','handlebars')
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
+app.use(express.static(__dirname + '/'));
 
 // Rotas
+app.get('/', function (req, res) {
+  res.render('tela-login')
+})
 
+app.get('/cadastro', function (req, res) {
+  res.render('cadastro')
+})
 
 app.get('/cad',function(req, res){
-	Post.findAll({order: [['id','ASC']]}).then(function(posts){ // Peguei todos os meus dados da tabela...
+	Post.findAll({order: [['id','ASC']]}).then(function(posts){ // Pega todos os dados da tabela.
 			console.log(posts)
 			res.render('cadastro',{posts: posts})
 	})		
@@ -31,7 +40,7 @@ app.get('/cad',function(req, res){
 //})
 
 
-// Gravando as informações no banco de dados
+// Gravando as informações no banco de dados.
 
 app.post('/add',function(req, res){
 Post.create({
@@ -54,9 +63,40 @@ Post.create({
 
 app.get('/deletar/:id',function(req, res){
 	Post.destroy({where: {'id':req.params.id}}).then(function(){
-		res.send("Postagem deletada com sucesso!")
+		res.redirect('/cad')//res.send("Postagem deletada com sucesso!")
 	})
 })
+
+app.get('/editar/:id', function (req, res) {
+  Post.findOne({ id: req.params.id }).then((prod) => {
+    res.render('editar', { prod: prod})
+  }).catch(function (erro) {
+    res.send("Aluno Inexistente!")
+  })
+});
+
+// Editando 
+
+app.post('/editar', (req, res) => {
+  Post.findOne({ id: request.body.id }).then((prod) => {
+
+	cad.nome = req.body.nome,
+	cad.preco = req.body.preco,
+	cad.quantidade = req.body.qtd,
+	cad.setor = req.body.tipo,
+	cad.marcar = req.body.marca
+
+    prod.save().then(() => {
+      res.redirect('/prod')
+    }).catch((err) => {
+      res.send("Houve um erro ao editar produto")
+    })
+
+  }).catch((err) => {
+    res.send("Erro ao editar produto")
+  })
+});
+
 
 app.listen(8081, function(){
 	console.log("Conectou");
